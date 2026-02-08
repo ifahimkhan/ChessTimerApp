4327a01e664c8656a3e036e68f7a18204d0975a1
ifahimkhan <khan.fahim95@hotmail.com>
2026-02-09 04:20:52 +0530

    If applied, this commit will update the versionCode to 5 and the versionName to 1.5.0.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.fahim.chesstimer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0"
+        versionCode = 5
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
